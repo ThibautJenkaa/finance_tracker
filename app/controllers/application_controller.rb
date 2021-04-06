b1cfcb8bbe7e48ec8c1db6511908778afefde540
Thibaut <thibaut@jenkaa.com>
2021-04-06 10:15:21 +0200

    update user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name])
+  end
 
   def after_sign_in_path_for(resource)
     my_portfolio_path # your path
---
+++ db/migrate/20210406073952_add_fisrt_last_name_to_users.rb
@@ -0,0 +1,6 @@
+class AddFisrtLastNameToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
